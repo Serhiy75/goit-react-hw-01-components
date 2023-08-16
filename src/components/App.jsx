9ebe4282193d6da70444532e7f51f04337a7928d
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-17 00:01:22 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
+import { Profile } from "./Profile/Profile";
+import user from '../data/user.json'
+import data from '../data/data.json';
+import { Statistics } from "./Statistics/Statistics";
+
 export const App = () => {
   return (
     <div
       style={{
+        flexDirection: 'column',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
@@ -10,7 +16,19 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+       <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}/>
+      <Statistics
+      title={'APLOAD STATS'}
+      stats={data}
+      />  
+
     </div>
   );
 };
+
+
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+import css from './Statistics.module.css'
+
+export const Statistics = ({title, stats}) => <section className={css.statistics}>
+<h2 className={css.title}>{title}</h2>
+
+<ul className={css.statlist}>
+    {stats.map(({id, label, percentage}) =>  <li key={id} className={css.item}>
+    <span className={css.label}>{label}</span>
+    <span className={css.percentage}>{percentage}</span>
+  </li>)}
+ 
+</ul>
+</section>
